fec94d9661486e5a79411bb9f14cfc030099f1c3
KOKUMUbooker <bookerochieng20@outlook.com>
2026-01-29 18:00:31 +0300

    Add logic to fill up bannerDictionary with data read from banner file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.Collections.Generic;
 
 namespace ascii_art_dotnet;
 
@@ -9,10 +10,26 @@ class Program
         string banner = "standard";
         try 
         {
+            Console.WriteLine("Args : ",args);
+            
+            // Reading files
             IEnumerable<string> lines = File.ReadLines(@$"./BannerFiles/{banner}.txt");
 
+            int i = 0;
+            char curRune = ' ';
+            var bannerDict = new Dictionary<char,List<string>>();
+            bannerDict.Add(' ',new List<string>());
+            
             foreach (string line in lines) 
             {
+                if (line.Length == 0) continue;
+
+                bannerDict[curRune].Add(line);
+                if (i != 0 && i % 8 == 0) 
+                {
+                    curRune++;
+                    if (curRune <= 126) bannerDict.Add(curRune, new List<string>());
+                }
                 Console.WriteLine(line);
             }
         }
