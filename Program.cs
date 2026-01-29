3f47d1ce49652983da0bc312f6de1ef58e4cb2d5
KOKUMUbooker <bookerochieng20@outlook.com>
2026-01-29 17:30:48 +0300

    Starter logic for reading file contents

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
-﻿namespace ascii_art_dotnet;
+﻿using System.IO;
+
+namespace ascii_art_dotnet;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string banner = "standard";
+        try 
+        {
+            IEnumerable<string> lines = File.ReadLines(@$"./BannerFiles/{banner}.txt");
+
+            foreach (string line in lines) 
+            {
+                Console.WriteLine(line);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failed to read file {banner}.txt : ");
+            Console.WriteLine(e.Message);
+        }
     }
 }
